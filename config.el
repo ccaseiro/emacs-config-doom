08cbcbfc523ae602ca05d40c2efdcd0443e3cb66
Claudio Caseiro <ccaseiro@gmail.com>
2022-01-15 14:34:19 +0000

    add mapping leader "w f" 'ace-swap-window

--- config.el
+++ config.el
@@ -269,6 +269,7 @@ Refer to `org-agenda-prefix-format' for more information."
 
 ;; Add some Leader Keybindings
 (map! :leader "w a" 'ace-window)
+(map! :leader "w f" 'ace-swap-window)
 
 ;; Swap leader-: with leader-;
 (map! :leader ":" 'pp-eval-expression
